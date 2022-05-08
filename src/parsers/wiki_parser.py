96b6c6ff5ff639801d6cdde050199dbc15bc5be5
enzzzhe <enzzzhe@gmail.com>
2022-05-08 17:19:27 +0300

    kkk

--- src/parsers/wiki_parser.py
+++ src/parsers/wiki_parser.py
@@ -36,6 +36,7 @@ def file_search_flag(path):
         return True
     else:
         print('content.bin hasn\'t been found in directory: creating new one')
+        return False
 
 
 def wiki_parser(url: str, base_path: str):
