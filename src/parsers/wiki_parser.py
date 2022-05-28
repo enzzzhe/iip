563efe7496799dac6186145a039d1c19cb3e181c
enzzzhe <enzzzhe@gmail.com>
2022-05-28 08:46:43 +0300

    added list

--- src/parsers/wiki_parser.py
+++ src/parsers/wiki_parser.py
@@ -1,35 +1,39 @@
 """System module."""
 import hashlib
 import urllib.request
+import bs4
 import os
 import re
-import bs4
 
+
+def counter(my_text, word):
+    """Searching for words with map"""
+    return sum(map(lambda x:x == word, my_text))
 
 def url_search(url):
     """Searching for url.txt file """
     url_file_path = "url.txt"
-    # if not os.path.exists(url_file_path):
-    #     print('url.txt doesn\'t exist: creating new one')
-    #     url_file = open('url.txt', 'w', encoding="utf8")
-    # else:
-    #     print('url.txt has been found')
-    #     url_file = open('url.txt', 'r+', encoding="utf8")
-    with open('url.txt', 'r+', encoding="utf8") as url_file:
 
-        if not (os.stat(url_file_path)).st_size:
-            print('url.txt is empty: creating new line with current url input')
-            url_file.write(url + '\n')
+    if not os.path.exists(url_file_path):
+        print('url.txt doesn\'t exist: creating new one')
+        url_file = open('url.txt', 'w')
+    else:
+        print('url.txt has been found')
+        url_file = open('url.txt', 'r+')
+
+    if not (os.stat(url_file_path)).st_size:
+        print('url.txt is empty: creating new line with current url input')
+        url_file.write(url + '\n')
+        return
+
+    for line in url_file:
+        if line == url:
+            print('current url input has been found in url.txt')
             return
 
-        for line in url_file:
-            if line == url:
-                print('current url input has been found in url.txt')
-                return
-
-        url_file.write(url + '\n')
-        print('url input hasn\'t been found in file: creating new line with current url input')
-        return
+    url_file.write(url + '\n')
+    print('current url input hasn\'t been found in url.txt: creating new line with current url input')
+    return
 
 
 def file_search_flag(path):
@@ -37,8 +41,9 @@ def file_search_flag(path):
     if os.path.exists(path):
         print('content.bin has been found')
         return True
-    print('content.bin hasn\'t been found in directory: creating new one')
-    return False
+    else:
+        print('content.bin hasn\'t been found in directory: creating new one')
+        return False
 
 
 def wiki_parser(url: str, base_path: str):
@@ -57,27 +62,106 @@ def wiki_parser(url: str, base_path: str):
     content_file_exists = file_search_flag(directory_path)
 
     if not content_file_exists:
-        with open(directory_path, 'wb', encoding="utf8") as content:
-            with urllib.request.urlopen(url, 'r+') as page_content:
-                soup = bs4.BeautifulSoup(page_content, features="html.parser")
+        with open(directory_path, 'wb') as content:
+            page_content = urllib.request.urlopen(url).read()
+            soup = bs4.BeautifulSoup(page_content, features="html.parser")
 
+            #content.write(soup)
+            # суп2 для выделения ссылок
+            soup2 = soup
+            page_urls = ''
             page_text = ''
 
             for paragraph in soup.find_all("p"):
                 page_text += paragraph.text
 
+            for paragraph in soup2.find_all("a"):
+                temporary = paragraph.get('href', None)
+                if temporary is not None:
+                    page_urls = page_urls + ' ' + temporary
+
+            page_urls = page_urls.split()
+            page_urls1 = []
+
+            for current_url in page_urls:
+                if not current_url.find('wikipedia.org') == -1:
+                    page_urls1.append(current_url)
+
+            page_text = re.sub(r'\[.*?\]+', '', page_text)
+
+            content.write(page_text.encode())
+            content.close()
+    else:
+        with open(directory_path, 'wb') as content:
+            page_content = urllib.request.urlopen(url).read()
+            soup = bs4.BeautifulSoup(page_content, features="html.parser")
+
+            content.write(soup)
+            # суп2 для выделения ссылок
+            soup2 = soup
+            page_text = ''
+            page_urls = ''
+
+            for paragraph in soup.find_all("p"):
+                page_text += paragraph.text
+
+            for paragraph in soup2.find_all("a"):
+                temporary = paragraph.get('href', None)
+                if temporary is not None:
+                    page_urls = page_urls + ' ' + temporary
+
+            page_urls = page_urls.split()
+            page_urls1 = []
+
+            for current_url in page_urls:
+                if not current_url.find('wikipedia.org') == -1:
+                    page_urls1.append(current_url)
+
+
             page_text = re.sub(r'\[.*?\]+', '', page_text)
 
             content.write(page_text.encode())
 
-    with open(directory_path, 'r', encoding="utf8") as content_file:
-        content_text = (content_file.read()).split()
+
+    with open(directory_path, 'r') as content_file:
+        content_text = (content_file.read())
+        content_file.close()
 
     directory_path = directory_path.replace('\\content.bin', '\\words.txt')
 
-    with open(directory_path, 'w', encoding="utf8") as words_file:
-        for word in content_text:
-            words_file.write(word + '\n')
+    content_text = content_text.replace(',', '')
+    content_text = content_text.replace('.', '')
+    content_text = content_text.replace('?', '')
+    content_text = content_text.replace('!', '')
+    content_text = content_text.replace('(', '')
+    content_text = content_text.replace(')', '')
+    content_text = content_text.replace(':', '')
+    content_text = content_text.replace('"', '')
+    content_text = content_text.replace('\'', '')
+    content_text = content_text.lower()
+    content_text = content_text.split()
+
+    dictionary = {}
+    for line in content_text:
+        dictionary[line] = counter(content_text, line)
+    #print(dictionary)
 
 
-wiki_parser('https://en.wikipedia.org/wiki/Winston_Churchill', 'parsers')
+    directory_path = directory_path.replace('\\content.bin', '\\words.txt')
+
+
+    with open(directory_path, 'w') as words_file:
+        for key, value in dictionary.items():
+            words_file.write('%s : %s \n' % (key, value))
+        words_file.close()
+
+    print(page_urls1)
+    return page_urls1
+
+
+
+link = input()
+directory_name = input()
+wiki_parser(link, directory_name)
+
+#https://en.wikipedia.org/wiki/Isaac_Newton
